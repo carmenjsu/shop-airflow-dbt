26cd4332228e5cf7151d49e30bc4c27ef332ee75
acer <carmenjssu@gmail.com>
2025-08-06 16:52:09 +0100

    create fact table

---
+++ shop/models/fact/fct_order_delay.sql
@@ -0,0 +1,49 @@
+WITH orders AS (
+    SELECT
+        order_id,
+        customer_id,
+        order_date
+    FROM
+        {{ ref('stg_orders') }}
+),
+
+customers AS (
+    SELECT
+        customer_id,
+        name,
+        email
+    FROM
+        {{ ref('stg_customers') }}
+),
+
+shipments AS (
+    SELECT
+        shipment_id,
+        order_id,
+        status,
+        shipped_at,
+        delivered_at
+    FROM
+        {{ ref('stg_shipments') }}
+)
+
+SELECT 
+    o.order_id,
+    o.customer_id,
+    c.name AS customer_name,
+    c.email AS customer_email,
+    s.shipment_id,
+    s.status AS shipment_status,
+    s.shipped_at,
+    s.delivered_at,
+    CASE 
+        WHEN delivered_at IS NULL AND DATEDIFF(day, shipped_at, CURRENT_TIMESTAMP) > 2 THEN 'Delayed'
+        WHEN delivered_at IS NOT NULL AND DATEDIFF(day, shipped_at, delivered_at) > 2 THEN 'Delayed'
+        ELSE 'On Time'
+    END AS delayed_status
+FROM
+    orders o
+LEFT JOIN
+    customers c ON o.customer_id = c.customer_id
+LEFT JOIN
+    shipments s ON o.order_id = s.order_id
